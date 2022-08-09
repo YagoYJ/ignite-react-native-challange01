0b651aa74b115e71bbe7cc87e77f288411875b25
yago.juan <yago.juan@teclat.com.br>
2022-08-09 16:16:38 -0300

    Finish the challange

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,49 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { Header } from '../components/Header';
-import { Task, TasksList } from '../components/TasksList';
-import { TodoInput } from '../components/TodoInput';
+import { Header } from "../components/Header";
+import { Task, TasksList } from "../components/TasksList";
+import { TodoInput } from "../components/TodoInput";
 
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks((oldTasks) => [
+      ...oldTasks,
+      {
+        id: Math.random() * 10,
+        done: false,
+        title: newTaskTitle,
+      },
+    ]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const newTasksArray = tasks.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          done: !task.done,
+        };
+      } else {
+        return task;
+      }
+    });
+
+    setTasks(newTasksArray);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    let newTasksArray: Task[] = [];
+
+    tasks.map((task) => {
+      if (task.id !== id) {
+        newTasksArray.push(task);
+      }
+    });
+
+    setTasks(newTasksArray);
   }
 
   return (
@@ -26,18 +52,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: "#EBEBEB",
+  },
+});
